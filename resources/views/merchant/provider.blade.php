a0e2c0f38c67d951ead50231930ed834a7c3a5c4
ahmedsaoud31 <ahmedsaoud31@gmail.com>
2019-01-18 16:53:39 -0800

    Update provider.blade.php
    update provider view to show action under provider service

--- resources/views/merchant/provider.blade.php
+++ resources/views/merchant/provider.blade.php
@@ -14,6 +14,12 @@
                 <div class="card pd-25 bd-0 shadow-base">
                     <img src="{{url('backend/services/')}}" class="img-fluid" alt="">
                     <a href="{{route('merchant.service', $service->id)}}" class="btn btn-success btn-oblong pd-x-25 pd-y-12 tx-11 tx-mont tx-uppercase tx-13-force tx-bold d-block">{{$service->name}}</a>
+                    <hr>
+                    <div>
+                        @foreach($service->apis as $api)
+                            <a href="{{ route('merchant.api.show', $api->id)}}" class="btn btn-primary">{{$api->name}}</a>
+                        @endforeach
+                    </div>
                 </div>
             @endforeach
         </div>
