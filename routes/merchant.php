7283404582f0cda57301a87e5734c5ecf455e4cf
ahmedsaoud31 <ahmedsaoud31@gmail.com>
2019-01-18 16:55:08 -0800

    Update merchant.php
    add api route

--- routes/merchant.php
+++ routes/merchant.php
@@ -4,8 +4,12 @@
 Route::get('/', 'MainController@index')->name('home');
 Route::get('lang', 'MainController@change_lang')->name('lang');
 
+Route::post('api/pay', 'ApiController@pay')->name('api.pay');
+Route::resource('api', 'ApiController');
 
 Route::get('/service/{id}', 'ServiceController@index')->name('service');
 Route::get('/provider/{id}', 'ServiceController@provider')->name('provider');
 Route::get('lock', 'Auth\LoginController@lock')->name('lock');
+
+
 
