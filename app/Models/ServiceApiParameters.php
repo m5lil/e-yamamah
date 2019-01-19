33fc4554798bc95bb7b958bfc78dd949ea150776
ahmedsaoud31 <ahmedsaoud31@gmail.com>
2019-01-18 16:49:53 -0800

    Update ServiceApiParameters.php
    add name attribute with translation
    add belongsTo relation to ServiceApi

--- app/Models/ServiceApiParameters.php
+++ app/Models/ServiceApiParameters.php
@@ -12,4 +12,13 @@ class ServiceApiParameters extends BaseModel
         'name'   => 'required',
     ];
 
+    function getNameAttribute()
+    {
+        return json_decode($this->attributes['name'], true)[app()->getLocale()];
+    }
+
+    public function service_api()
+    {
+        return $this->belongsTo(ServiceApi::class);
+    }
 }
